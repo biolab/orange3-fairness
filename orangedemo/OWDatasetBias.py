7b4fa4c83447766ffdc4c3ddf3119aaf62a2cccf
ZanMervic <zan.mervic@gmail.com>
2023-06-27 11:09:36 +0200

    Working DatasetBias widget

--- orangedemo/OWDatasetBias.py
+++ orangedemo/OWDatasetBias.py
@@ -12,13 +12,20 @@ from Orange.data import Table, Domain, DiscreteVariable
 from aif360.datasets import StandardDataset
 from aif360.metrics import BinaryLabelDatasetMetric
 
+import pandas as pd
+
 class OWDatasetBias(OWWidget):
     name = "Dataset Bias"
     description = "Computes the bias of a dataset."
     # icon = "icons/bias.svg"
     # priority = 0
 
+    want_control_area = False
+    resizing_enabled = False
+
     standardDataset = None
+    unprivileged_groups = None
+    privileged_groups = None
     
     class Inputs:
         data = Input("Data", Table)
@@ -26,32 +33,74 @@ class OWDatasetBias(OWWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.disparate_impact = 0.0
+        self.statistical_parity_difference = 0.0
         self._data: Optional[Table] = None
+
+        box = gui.vBox(self.mainArea, "Bias")
+        self.DI_label = gui.label(box, self, "No data detected.")
+        self.SPD_label = gui.label(box, self, "No data detected.")
 
     @Inputs.data
     def set_data(self, data: Optional[Table]) -> None:
-        # self.closeContext()
-
-        if not data:
+        if not data or not "favorable_class_value" in data.attributes or not "protected_attribute" in data.attributes or not "privileged_PA_values" in data.attributes or data.domain.class_var.name != "y":
             return
         
         self._data = data
-        # self.openContext(data)
 
+        # Convert Orange data to aif360 StandardDataset
         self.convertToStandardDataset(data)
 
+        # Compute bias
+        self.computeBias()
+
     def convertToStandardDataset(self, data) -> None:
-        # Convert Orange data to aif360 dataset
-        df = data.to_pandas_dfs()
+        # Convert Orange data to aif360 dataset, it returns a touple xdf, ydf, mdf
+        xdf, ydf, mdf = data.to_pandas_dfs()
+        # Merge xdf and ydf TODO: Check if I need to merge mdf
+        df = ydf.merge(xdf, left_index=True, right_index=True)
+
+        # TODO: Change this so it reads these values from the domain
         favorable_class_value = data.attributes["favorable_class_value"]
         protected_attribute = data.attributes["protected_attribute"]
         privileged_PA_values = data.attributes["privileged_PA_values"]
 
+        # Convert the favorable_class_value and privileged_PA_values from their string representation to their integer representation
+        # We need to do this because when we convert the Orange table to a pandas dataframe all categorical variables are ordinal encoded
+
+        # Get the values for the attributes
+        class_values = data.domain["y"].values
+        protected_attribute_values = data.domain[protected_attribute].values
+
+        # Get the index of the favorable_class_value and privileged_PA_values in the list of values, this is the ordinal representation
+        favorable_class_value_ordinal = class_values.index(favorable_class_value)
+        privileged_PA_values_ordinal = [protected_attribute_values.index(value) for value in privileged_PA_values]
+        unprivileged_PA_values_ordinal = [i for i in range(len(protected_attribute_values)) if i not in privileged_PA_values_ordinal]
+
+        # Create the StandardDataset, this is the dataset that aif360 uses
+        # df: a pandas dataframe containing all the data
+        # label_name: the name of the class variable
+        # favorable_classes: the values of the class variable that are considered favorable
+        # protected_attribute_names: the name of the protected attribute
+        # privileged_classes: the values of the protected attribute that are considered privileged (in this case they are ordinal encoded)
         self.standardDataset = StandardDataset(
             df = df,
             label_name = "y",
-            favorable_classes = [favorable_class_value],
+            favorable_classes = [favorable_class_value_ordinal],
             protected_attribute_names = [protected_attribute],
-            privileged_classes = [privileged_PA_values]
+            privileged_classes = [privileged_PA_values_ordinal],
+            # categorical_features = discrete_variables,
         )
-        
+
+        # Create the privileged and unprivileged groups
+        # The format is a list of dictionaries, each dictionary contains the name of the protected attribute and the ordinal value of the privileged/unprivileged group
+        self.privileged_groups = [{protected_attribute: ordinal_value} for ordinal_value in privileged_PA_values_ordinal]
+        self.unprivileged_groups = [{protected_attribute: ordinal_value} for ordinal_value in unprivileged_PA_values_ordinal]
+
+    # Compute the bias of the dataset (disparate impact and statistical parity difference)
+    def computeBias(self) -> None:
+        dataset_metric = BinaryLabelDatasetMetric(self.standardDataset, self.unprivileged_groups, self.privileged_groups)
+        self.disparate_impact = dataset_metric.disparate_impact()
+        self.statistical_parity_difference = dataset_metric.statistical_parity_difference()
+        self.DI_label.setText(f"Disparate Impact (ideal = 1): {self.disparate_impact}")
+        self.SPD_label.setText(f"Statistical Parity Difference (ideal = 0): {self.statistical_parity_difference}")

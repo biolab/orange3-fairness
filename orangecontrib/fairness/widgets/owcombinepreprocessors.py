ce290d4857b7345dd453a737c759e2aa49eb4b0a
ZanMervic <zan.mervic@gmail.com>
2023-07-25 12:08:40 +0200

    Weighted logistic regression and fixes to combine preprocessors

--- orangecontrib/fairness/widgets/owcombinepreprocessors.py
+++ orangecontrib/fairness/widgets/owcombinepreprocessors.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from Orange.widgets import gui
 from Orange.widgets.widget import Input, Output, OWWidget
 from Orange.preprocess.preprocess import Preprocess, PreprocessorList
 
@@ -10,6 +11,9 @@ class OWCombinePreprocessors(OWWidget):
     # icon = "icons/combine_preprocessors.svg"
     # priority = 0
 
+    want_control_area = False
+    resizing_enabled = False
+
     class Inputs:
         first_preprocessor = Input("First Preprocessor", Preprocess)
         second_preprocessor = Input("Second Preprocessor", Preprocess)
@@ -19,7 +23,16 @@ class OWCombinePreprocessors(OWWidget):
 
     def __init__(self):
         self.preprocessor_list = []
+        self.first_preprocessor = None
+        self.second_preprocessor = None
+        
         super().__init__()
+
+        box = gui.vBox(self.mainArea, "Info")
+        gui.widgetLabel(
+            box,
+            "This widgets allows you to combine two preprocessors into one and use it as input for other widgets.",
+        )
 
 
     @Inputs.first_preprocessor
